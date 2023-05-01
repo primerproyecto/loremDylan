dcd7ffad152990dbe9d693910e27d564abe3f2e3
Juan Tuñon <primerproyecto@gmail.com>
2023-05-01 19:20:38 +0200

    agrego endpoint

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const data = require("../data/bobDylanSongs.json");
 
-app.get("/", (req, res) => {
+app.get("/api/v1/all", (req, res) => {
   console.log("estamos escuchando", data);
   res.json(data);
 });
