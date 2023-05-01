0e006a9439a459e23105521a7f350199616766e4
Juan Tuñon <primerproyecto@gmail.com>
2023-05-01 20:31:27 +0200

    agrego cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const data = require("../data/bobDylanSongs.json");
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.get("/api/v1/all", (req, res) => {
   console.log("estamos escuchando", data);
   res.json(JSON.stringify(data));
