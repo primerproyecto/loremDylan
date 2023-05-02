f98a5442eecf4a4fbe07a25cca6c7cb39d497b2c
Juan Tuñon <primerproyecto@gmail.com>
2023-05-02 12:37:43 +0200

    agrego index.html

--- src/server.js
+++ src/server.js
@@ -1,26 +1,28 @@
 const express = require("express");
-const app = express();
+
 const cors = require("cors");
 const path = require("path");
 
 const data = require("../data/bobDylanSongs.json");
 
-const filteredData = data.slice(0, 10);
+const filteredData = data.slice(0, 2);
+
+const app = express();
 app.use(
   cors({
     origin: "*",
   })
 );
 app.get("/api/v1/all", (req, res) => {
-  console.log("estamos escuchando", data);
   /*  res.json(JSON.stringify(data)); */
-  res.json({ filteredData });
+  res.json({ "Choo Choo": "Welcome to your Express app 🚅" });
 });
 
 app.get("/", (req, res) => {
-  console.log("estamos escuchando", data);
   /* res.json({ titulo: "cancion" }); */
   res.sendFile(path.resolve(__dirname, "index.html"));
 });
-
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`loremDylan app listening on port ${port}`);
+});
