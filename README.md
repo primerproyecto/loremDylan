0f3b9fa6900ec70c0c8f7731304a66cc6b2df4d8
Juan Tuñon <primerproyecto@gmail.com>
2023-05-02 16:36:06 +0200

    agrego endpoints

--- README.md
+++ README.md
@@ -15,9 +15,43 @@ lyrics: 'Texto en inglés'
 cover: 'url de la portada del disco en el que fue publicado'
 ```
 
+## endpoints
+
+En su versión primera esta api cuenta con los siguientes endopoints del tipo get. Por ahora sólamente se pueden leer el fichero.
+
+### Todas las canciones sin paginar.
+
+`````
+https://loremdylan-production.up.railway.app/api/v1/all
+`````
+
+### Todas las canciones paginadas de 10 en 10
+
+`````
+https://loremdylan-production.up.railway.app/api/v1/paginated
+`````
+
+`````
+https://loremdylan-production.up.railway.app/api/v1/paginated/?page=NUMBER
+`````
+
+### 1O Canciones del dylan
+
+`````
+https://loremdylan-production.up.railway.app/api/v1/10/?page=NUMBER
+`````
+
+### Frase aleatoria de las canciones del dylan
+
+`````
+https://loremdylan-production.up.railway.app/api/v1/frase
+`````
+
+
 
 ## Webscraping
 
-Desde la consola y con el comando ```npm run dylanToJson``` nos generará un archivo en formato ```json``` con las canciones de bobDylan en la carpeta data de este respositorio.
+Esta api está construída desde los datos que se encuentran en el fichero ```data/bobDylanSongs.json```.
+Si se desea actualizar el fichero se puede desde la consola y con el comando ```npm run dylanToJson``` nos generará un archivo en formato ```json``` con las canciones de bobDylan en la carpeta data de este respositorio.
 
 ![lista de canicones](songsPage.png)
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const _ = require("lodash");
 
 const cors = require("cors");
 const path = require("path");
@@ -17,17 +18,37 @@ app.use(express.json());
 
 /* TO GET ALL SONGS FROM JSON FILE */
 app.get("/api/v1/all", (req, res) => {
-  res.json([data]);
+  res.json({ items: data.length, data });
+});
+
+/* TO GET ALL SONGS PAGINATED FROM JSON FILE */
+app.get("/api/v1/paginated", (req, res) => {
+  const itemsPerPage = 10; // número de elementos por página
+  const array = data; // array original
+  const pageNumber = req.query.page || 1; // página actual (por defecto es la primera página)
+
+  const paginatedArray = _.chunk(array, itemsPerPage)[pageNumber - 1] || []; // array paginado
+
+  res.json(paginatedArray);
 });
 /* TO GET 10 SONGS FROM JSON FILE */
 app.get("/api/v1/10", (req, res) => {
   const filteredData = data.slice(0, 10);
-  res.json([filteredData]);
+  res.json(filteredData);
 });
 
-app.get("/", (req, res) => {
-  /* res.json({ titulo: "cancion" }); */
-  res.sendFile(path.resolve(__dirname, "index.html"));
+/* FRASE ALEATORIA DE UNA CACION DE DYLAN */
+app.get("/api/v1/frase", (req, res) => {
+  const randomNumber = Math.floor(Math.random() * data.length);
+  const frase = data[randomNumber].lyrics;
+  const cancion = data[randomNumber].song;
+
+  const words = frase.split(" ");
+  const slicedWords = words.slice(0, 20);
+  const slicedText =
+    slicedWords.join(" ").replace(/(<br>|<br\/>|\n)/gi, " ") + " ...";
+
+  res.json({ slicedText, cancion });
 });
 
 app.listen(port, () => {
